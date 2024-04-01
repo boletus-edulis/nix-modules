40b480dd5d0e29cf06fabb351246db7755dfe668
boletus-edulis <the.boletus.edulis@gmail.com>
2024-04-01 20:49:20 +0200

    set wayland variables

--- wayland.nix
+++ wayland.nix
@@ -72,6 +72,9 @@
       ];
       home.sessionVariables = {
         WLR_NO_HARDWARE_CURSORS = 1;
+        QT_QPA_PLATFORM = "wayland";
+        GDK_BACKEND = "wayland";
+        MOZ_ENABLE_WAYLAND = 1;
       };
       home.keyboard = {
         layout = "us";
