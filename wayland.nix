bb8ec4045b828ac4cfdfc91ac7481f64601baad0
boletus-edulis <the.boletus.edulis@gmail.com>
2024-04-01 21:14:43 +0200

    rofi does not pass command line parameters. use fuzzel.

--- wayland.nix
+++ wayland.nix
@@ -233,7 +233,7 @@
               "${modifier}+l" = "exec ${pkgs.swaylock}/bin/swaylock -fF -u -c 000000";
               "${modifier}+Return" = "exec ${terminal}";
               "${modifier}+Shift+q" = "kill";
-              "${modifier}+d" = "exec ${pkgs.rofi}/bin/rofi -show drun";
+              "${modifier}+d" = "exec ${pkgs.fuzzel}/bin/fuzzel";
 
               "${modifier}+${left}" = "focus left";
               "${modifier}+${down}" = "focus down";
