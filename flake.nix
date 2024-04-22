2ca464c69ff89b82cc592b01bc90814cc223ea4f
boletus-edulis <the.boletus.edulis@gmail.com>
2024-04-23 01:07:55 +0200

    add hydraJob

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-lib.url = "github:NixOS/nixpkgs/nixos-unstable?dir=lib";
 
     hydra-test.url = "github:boletus-edulis/hydra-test";
     hydra-test.inputs.nixpkgs.follows = "nixpkgs";
@@ -20,6 +21,29 @@
       wayland = import ./wayland.nix;
 
       ribes-uva-crispa = import ./x13s.nix;
+    };
+
+    nixosConfigurations.dummySystem = nixpkgs.lib.nixosSystem rec {
+      system = "aarch64-linux";
+      specialArgs = {
+        inherit inputs;
+        inherit system;
+        username = "Us0r";
+        cpkgs = inputs.hydra-test.packages.aarch64-linux;
+      };
+      modules = [
+        home-manager.nixosModules.home-manager
+        self.nixosModules.baseline-configuration
+        self.nixosModules.enable-unfree
+        self.nixosModules.ribes-uva-crispa
+        self.nixosModules.base-home
+        self.nixosModules.wayland
+      ];
+    };
+
+    hydraJobs = {
+      dummySystem = inputs.nixpkgs.lib.customisation.hydraJob
+        self.nixosConfigurations.dummySystem.config.system.build.toplevel;
     };
   };
 }
