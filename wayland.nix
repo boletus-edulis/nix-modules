0b394efef1dcba38d23e8289c3fbf74b813b0d43
boletus-edulis <the.boletus.edulis@gmail.com>
2024-04-23 23:46:48 +0200

    fix service name

--- wayland.nix
+++ wayland.nix
@@ -59,7 +59,7 @@
     services.avahi.enable = true;
     # Important to resolve .local domains of printers, otherwise you get an error
     # like  "Impossible to connect to XXX.local: Name or service not known"
-    services.avahi.nssmdns = true;
+    services.avahi.nssmdns4 = true;
 
     services.syncthing = {
       enable = true;
