98aaa11f1ed1b2080422e3a0be5b57f8a73a2129
boletus-edulis <the.boletus.edulis@gmail.com>
2024-06-27 15:43:33 +0200

    wrap in container, not tested yet

--- simple-router.nix
+++ simple-router.nix
@@ -32,9 +32,7 @@ in
 
     systemd.network.networks."10-simple-router-internal" = {
       matchConfig.Name = systemd.network.netdevs."10-simple-router-internal".netdevConfig.Name;
-      addresses = [
-        { Address = "172.16.0.1/24"; }
-      ];
+      addresses = [ { Address = "172.16.0.1/24"; } ];
     };
 
     systemd.network.networks."10-simple-router-nic" = {
@@ -42,71 +40,71 @@ in
       matchConfig.Name = "${cfg.internalInterface}";
     };
 
-    networking.nat.enable = true;
-    networking.nat.externalInterface = "${cfg.externalInterface}";
-    networking.nat.internalInterfaces = [
-      systemd.network.netdevs."10-simple-router-internal".netdevConfig.Name
-    ];
+    networking.nat = {
+      enable = true;
+      externalInterface = "${cfg.externalInterface}";
+      internalInterfaces = [
+        systemd.network.netdevs."10-simple-router-internal".netdevConfig.Name
+      ];
+    };
 
-    services.kea = {
-      dhcp4 = {
-        enable = true;
-        extraArgs = [ "-d" ];
-        settings = {
-          interfaces-config = {
-            interfaces = [ systemd.network.netdevs."10-simple-router-internal".netdevConfig.Name ];
-          };
-          lease-db = {
-            name = "/var/lib/kea/dhcp4.leases";
-            persist = true;
-            type = "memfile";
-          };
-          option-data = [
-            {
-              name = "domain-name-servers";
-              space = "dhcp4";
-              data = "172.16.0.1";
-            }
-            {
-              name = "routers";
-              space = "dhcp4";
-              data = "172.16.0.1";
-            }
-          ];
-          subnet4 = [
-            {
-              id = 1;
-              subnet = "172.16.0.1/24";
-              pools = [
-                {
-                  pool = "172.16.0.100 - 172.16.0.200";
+    containers."simple-router" = {
+      interfaces = [ systemd.network.netdevs."10-simple-router-internal".netdevConfig.Name ];
+      tmpfs = [ "/var" ];
+
+      config = { config, pkgs, lib, ... }: {
+        networking.useHostResolvConf = lib.mkForce false;
+        services.resolved.enable = true;
+
+        services.kea = {
+          dhcp4 = {
+            enable = true;
+            extraArgs = [ "-d" ];
+            settings = {
+              interfaces-config = {
+                interfaces = [ systemd.network.netdevs."10-simple-router-internal".netdevConfig.Name ];
+              };
+              lease-db = {
+                name = "/var/lib/kea/dhcp4.leases";
+                persist = true;
+                type = "memfile";
+              };
+              option-data = [
+                { name = "domain-name-servers";
+                  space = "dhcp4";
+                  data = "172.16.0.1";
+                }
+                { name = "routers";
+                  space = "dhcp4";
+                  data = "172.16.0.1";
                 }
               ];
-            }
-          ];
+              subnet4 = [
+                { id = 1;
+                  subnet = "172.16.0.1/24";
+                  pools = [ { pool = "172.16.0.100 - 172.16.0.200"; } ];
+                }
+              ];
+            };
+          };
         };
-      };
-    };
 
-    services.unbound = {
-      enable = true;
-      settings = {
-        remote-control.control-enable = true;
-        server = {
-          interface = [ systemd.network.netdevs."10-simple-router-internal".netdevConfig.Name ];
-          verbosity = 2;
-        };
-        forward-zone = [
-          {
-            name = ".";
-            forward-addr = [
-              "1.1.1.1@853#cloudflare-dns.com"
-              "1.0.0.1@853#cloudflare-dns.com"
+        services.unbound = {
+          enable = true;
+          settings = {
+            remote-control.control-enable = true;
+            server = {
+              interface = [ systemd.network.netdevs."10-simple-router-internal".netdevConfig.Name ];
+              verbosity = 2;
+            };
+            forward-zone = [
+              { name = ".";
+                forward-addr = [ "1.1.1.1@853#cloudflare-dns.com" "1.0.0.1@853#cloudflare-dns.com" ];
+              }
             ];
-          }
-        ];
+          };
+        };
       };
     };
-
   };
 }
