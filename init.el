048aa9ece0908388417c9e6cb03336eea09b865e
boletus-edulis <the.boletus.edulis@gmail.com>
2025-04-14 22:20:16 +0200

    disable lsp and elpy

--- init.el
+++ init.el
@@ -94,27 +94,27 @@
 
   (leaf python :custom ((python-shell-interpreter . "python3")))
 
-  (leaf elpy
-    :ensure t
-    :custom ((elpy-rpc-backend . "jedi")
-	     (elpy-rpc-virtualenv-path . 'global)
-	     (elpy-rpc-python-command . "python3")
-	     (elpy-modules . (elpy-module-sane-defaults
-			      elpy-module-company
-			      elpy-module-eldoc
-			      elpy-module-highlight-indentation
-			      elpy-module-pyvenv
-			      elpy-module-yasnippet
-			      elpy-module-django)))
-    :hook python-mode-hook
-    :config
-    (leaf jedi
-      :ensure t)
-    (add-hook 'elpy-mode-hook (lambda ()
-                                (add-hook 'before-save-hook
-                                          'elpy-black-fix-code nil t)))
-    (elpy-enable)
-    )
+  ;;(leaf elpy
+  ;;  :ensure t
+  ;;  :custom ((elpy-rpc-backend . "jedi")
+  ;;           (elpy-rpc-virtualenv-path . 'global)
+  ;;           (elpy-rpc-python-command . "python3")
+  ;;           (elpy-modules . (elpy-module-sane-defaults
+  ;;      		      elpy-module-company
+  ;;      		      elpy-module-eldoc
+  ;;      		      elpy-module-highlight-indentation
+  ;;      		      elpy-module-pyvenv
+  ;;      		      elpy-module-yasnippet
+  ;;      		      elpy-module-django)))
+  ;;  :hook python-mode-hook
+  ;;  :config
+  ;;  (leaf jedi
+  ;;    :ensure t)
+  ;;  (add-hook 'elpy-mode-hook (lambda ()
+  ;;                              (add-hook 'before-save-hook
+  ;;                                        'elpy-black-fix-code nil t)))
+  ;;  (elpy-enable)
+  ;;  )
 
   (leaf flycheck
     :ensure t
@@ -175,42 +175,42 @@
 
   (leaf rust-auto-use :ensure t)
 
-  (leaf lsp-mode
-    :ensure t
-    :hook (prog-mode-hook . lsp-mode)
-    :custom `((gc-cons-threshold . 300000000)
-    	      (read-process-output-max . ,(* 10 1024 1024)))
-    :config
-    (defun lsp-booster--advice-json-parse (old-fn &rest args)
-      "Try to parse bytecode instead of json."
-      (or
-       (when (equal (following-char) ?#)
-    	 (let ((bytecode (read (current-buffer))))
-           (when (byte-code-function-p bytecode)
-             (funcall bytecode))))
-       (apply old-fn args)))
-
-    (advice-add (if (progn (require 'json)
-                           (fboundp 'json-parse-buffer))
-                    'json-parse-buffer
-                  'json-read)
-    		:around
-    		#'lsp-booster--advice-json-parse)
-
-    (defun lsp-booster--advice-final-command (old-fn cmd &optional test?)
-      "Prepend emacs-lsp-booster command to lsp CMD."
-      (let ((orig-result (funcall old-fn cmd test?)))
-    	(if (and (not test?)                             ;; for check lsp-server-present?
-    		 (not (file-remote-p default-directory)) ;; see lsp-resolve-final-command, it would add extra shell wrapper
-    		 lsp-use-plists
-    		 (not (functionp 'json-rpc-connection))  ;; native json-rpc
-    		 (executable-find "emacs-lsp-booster"))
-            (progn
-    	      (message "Using emacs-lsp-booster for %s!" orig-result)
-    	      (cons "emacs-lsp-booster" orig-result))
-          orig-result)))
-
-    (advice-add 'lsp-resolve-final-command :around #'lsp-booster--advice-final-command))
+;;  (leaf lsp-mode
+;;    :ensure t
+;;    :hook (prog-mode-hook . lsp-mode)
+;;    :custom `((gc-cons-threshold . 300000000)
+;;    	      (read-process-output-max . ,(* 10 1024 1024)))
+;;    :config
+;;    (defun lsp-booster--advice-json-parse (old-fn &rest args)
+;;      "Try to parse bytecode instead of json."
+;;      (or
+;;       (when (equal (following-char) ?#)
+;;    	 (let ((bytecode (read (current-buffer))))
+;;           (when (byte-code-function-p bytecode)
+;;             (funcall bytecode))))
+;;       (apply old-fn args)))
+;;
+;;    (advice-add (if (progn (require 'json)
+;;                           (fboundp 'json-parse-buffer))
+;;                    'json-parse-buffer
+;;                  'json-read)
+;;    		:around
+;;    		#'lsp-booster--advice-json-parse)
+;;
+;;    (defun lsp-booster--advice-final-command (old-fn cmd &optional test?)
+;;      "Prepend emacs-lsp-booster command to lsp CMD."
+;;      (let ((orig-result (funcall old-fn cmd test?)))
+;;    	(if (and (not test?)                             ;; for check lsp-server-present?
+;;    		 (not (file-remote-p default-directory)) ;; see lsp-resolve-final-command, it would add extra shell wrapper
+;;    		 lsp-use-plists
+;;    		 (not (functionp 'json-rpc-connection))  ;; native json-rpc
+;;    		 (executable-find "emacs-lsp-booster"))
+;;            (progn
+;;    	      (message "Using emacs-lsp-booster for %s!" orig-result)
+;;    	      (cons "emacs-lsp-booster" orig-result))
+;;          orig-result)))
+;;
+;;    (advice-add 'lsp-resolve-final-command :around #'lsp-booster--advice-final-command))
 
   (leaf lsp-nix
     :config
