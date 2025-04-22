0dc76577ef2fbeda8f10c7a868a295c852fcbd3b
boletus-edulis <the.boletus.edulis@gmail.com>
2025-04-22 14:24:24 +0200

    disable signal-desktop, openjdk packages broken

--- desktopX11.nix
+++ desktopX11.nix
@@ -111,7 +111,9 @@
     home-manager.users."${username}" = { pkgs, config, ... }: {
         home.packages = with pkgs; [
           swaylock helvum imhex flatpak virt-manager pavucontrol kdePackages.okular remmina
-          thunderbird firefox signal-desktop mumble chromium kdePackages.skanlite
+          thunderbird firefox
+          #signal-desktop
+          mumble chromium kdePackages.skanlite
         ];
         home.pointerCursor = {
           name = "Adwaita";
