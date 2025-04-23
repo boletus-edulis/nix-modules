db306cbcf1aed05ed8cb7c3e2b4059b490a5dfb1
boletus-edulis <the.boletus.edulis@gmail.com>
2025-04-23 22:15:12 +0200

    rework emacs

---
+++ emacs.nix
@@ -0,0 +1,39 @@
+{ config, pkgs, ... }:
+
+{
+  programs.bash = {
+    enable = true;
+    historySize = -1;
+    historyFileSize = -1;
+    bashrcExtra = let
+      dicts = [
+        pkgs.hunspellDicts.de-de
+        pkgs.hunspellDicts.de_DE
+        pkgs.hunspellDicts.en-us
+        pkgs.hunspellDicts.en_US
+        pkgs.hunspellDictsChromium.de-de
+        pkgs.hunspellDictsChromium.de_DE
+        pkgs.hunspellDictsChromium.en-us
+        pkgs.hunspellDictsChromium.en_US
+      ];
+      buildPath = dict: "${dict}/share/hunspell";
+    in ''
+      export DICPATH=${builtins.concatStringsSep ":" (builtins.map buildPath dicts)}
+    '';
+  };
+
+  programs.emacs = {
+    enable = true;
+    #package = pkgs.emacs-git-nox;
+    package = pkgs.emacs-nox;
+    extraPackages = epkgs: [
+      epkgs.vterm epkgs.treesit-grammars.with-all-grammars
+    ];
+  };
+
+  home.file.emacs-init-el = {
+    enable = true;
+    target = ".emacs.d/init.el";
+    source = ./init.el;
+  };
+}
--- home.nix
+++ home.nix
@@ -1,5 +1,15 @@
 { pkgs, config, username, inputs, ... }:
 
+let
+  emacs = import ./emacs.nix {};
+  home = {
+    packages = with pkgs; [
+      atool curl git conntrack-tools lsof file dnsutils tmux efibootmgr iotop
+      nftables tcpdump gdb emacs-lsp-booster ispell hunspell
+    ];
+    stateVersion = "23.11";
+  } // emacs.home;
+in
 {
   options = {};
   config = {
@@ -12,44 +22,8 @@
     home-manager.backupFileExtension = "backup";
     home-manager.users."${username}" = { pkgs, config, ... }: {
       #nixpkgs.overlays = [ inputs.emacs-overlay.overlays.default ];
-      programs.bash = {
-        enable = true;
-        historySize = -1;
-        historyFileSize = -1;
-        bashrcExtra = let
-          dicts = [
-            pkgs.hunspellDicts.de-de
-            pkgs.hunspellDicts.de_DE
-            pkgs.hunspellDicts.en-us
-            pkgs.hunspellDicts.en_US
-            pkgs.hunspellDictsChromium.de-de
-            pkgs.hunspellDictsChromium.de_DE
-            pkgs.hunspellDictsChromium.en-us
-            pkgs.hunspellDictsChromium.en_US
-          ];
-          buildPath = dict: "${dict}/share/hunspell";
-        in ''
-          export DICPATH=${builtins.concatStringsSep ":" (builtins.map buildPath dicts)}
-        '';
-      };
-      home.packages = with pkgs; [
-        atool curl git conntrack-tools lsof file dnsutils tmux efibootmgr iotop
-        nftables tcpdump gdb emacs-lsp-booster ispell hunspell
-      ];
-      home.stateVersion = "23.11";
-      programs.emacs = {
-        enable = true;
-        #package = pkgs.emacs-git-nox;
-        package = pkgs.emacs-nox;
-        extraPackages = epkgs: [
-          epkgs.vterm epkgs.treesit-grammars.with-all-grammars
-        ];
-      };
-      home.file.emacs-init-el = {
-        enable = true;
-        target = ".emacs.d/init.el";
-        source = ./init.el;
-      };
+      inherit home;
+      inherit (emacs) programs;
     };
   };
 }
