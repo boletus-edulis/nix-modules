b41eb392b5384f6b9021502edfe38b89fdd52fc3
boletus-edulis <the.boletus.edulis@gmail.com>
2025-07-28 13:56:54 +0200

    extend binfmt

--- x13s.nix
+++ x13s.nix
@@ -6,9 +6,8 @@
       (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.binfmt.emulatedSystems = [
-    "x86_64-linux"
-  ];
+  boot.binfmt.emulatedSystems = [ "x86_64-linux" "i686-linux" "armv7l-linux" ];
+  boot.binfmt.preferStaticEmulators = true;
 
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
