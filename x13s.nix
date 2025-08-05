9e05cdd038e9c19a2ae66bf8ddd8562532f73ae6
boletus-edulis <the.boletus.edulis@gmail.com>
2025-08-05 22:56:25 +0200

    disable static

--- x13s.nix
+++ x13s.nix
@@ -6,8 +6,8 @@
       (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.binfmt.emulatedSystems = [ "x86_64-linux" ]; #"i686-linux" "armv7l-linux" ];
-  boot.binfmt.preferStaticEmulators = true;
+  boot.binfmt.emulatedSystems = [ "x86_64-linux" "i686-linux" "armv7l-linux" ];
+  #boot.binfmt.preferStaticEmulators = true;
 
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
