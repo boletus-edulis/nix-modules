a239ec9254aec707d369f571000d42d80d3b82d2
boletus-edulis <the.boletus.edulis@gmail.com>
2026-02-18 22:50:16 +0100

    whats up with losing keybindings after starting helm?

--- init.el
+++ init.el
@@ -134,7 +134,8 @@
     :ensure t
     :bind (("M-x" . helm-M-x)
            ("C-x C-f" . helm-find-files)
-	   ("C-x b" . helm-buffers-list))
+	   ("C-x b" . helm-buffers-list)
+	   ("C-x k" . kill-buffer))
     :config (helm-mode 1))
 
   (use-package vterm
