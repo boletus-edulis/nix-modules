d7b4cd30f7f453ab2f2d00c74b76827f1fcc7eec
boletus-edulis <the.boletus.edulis@gmail.com>
2026-03-02 11:52:35 +0100

    actually pull in the font i want to use ...

--- desktop-general.nix
+++ desktop-general.nix
@@ -59,7 +59,7 @@
     };
 
     fonts.packages = [
-      pkgs.iosevka-bin
+      pkgs.iosevka-bin.override { variant = "SGr-IosevkaTerm"; }
       #cpkgs.iosevka-term
       pkgs.font-awesome
     ];
