db7339842f3a520add0a931fe2f50ae036ad6a86
boletus-edulis <the.boletus.edulis@gmail.com>
2026-03-18 21:54:32 +0100

    color theme

--- emacs.nix
+++ emacs.nix
@@ -10,107 +10,107 @@
     ];
     configFile = {
       text = ''
-        def color_theme [] {
-          return {
-            binary: '#ba8baf'
-            block: '#7cafc2'
-            cell-path: '#d8d8d8'
-            closure: '#86c1b9'
-            custom: '#f8f8f8'
-            duration: '#f7ca88'
-            float: '#ab4642'
-            glob: '#f8f8f8'
-            int: '#ba8baf'
-            list: '#86c1b9'
-            nothing: '#ab4642'
-            range: '#f7ca88'
-            record: '#86c1b9'
-            string: '#a1b56c'
+def color_theme [] {
+    return {
+        binary: '#747474'
+        block: '#686868'
+        cell-path: '#b9b9b9'
+        closure: '#868686'
+        custom: '#f7f7f7'
+        duration: '#a0a0a0'
+        float: '#7c7c7c'
+        glob: '#f7f7f7'
+        int: '#747474'
+        list: '#868686'
+        nothing: '#7c7c7c'
+        range: '#a0a0a0'
+        record: '#868686'
+        string: '#8e8e8e'
 
-            bool: {|| if $in { '#86c1b9' } else { '#f7ca88' } }
+        bool: {|| if $in { '#868686' } else { '#a0a0a0' } }
 
-            datetime: {|| (date now) - $in |
-              if $in < 1hr {
-                { fg: '#ab4642' attr: 'b' }
-              } else if $in < 6hr {
-                '#ab4642'
-              } else if $in < 1day {
-                '#f7ca88'
-              } else if $in < 3day {
-                '#a1b56c'
-              } else if $in < 1wk {
-                { fg: '#a1b56c' attr: 'b' }
-              } else if $in < 6wk {
-                '#86c1b9'
-              } else if $in < 52wk {
-                '#7cafc2'
-              } else { 'dark_gray' }
-            }
-
-            filesize: {|e|
-              if $e == 0b {
-                '#d8d8d8'
-              } else if $e < 1mb {
-                '#86c1b9'
-              } else {{ fg: '#7cafc2' }}
-            }
-
-            shape_and: { fg: '#ba8baf' attr: 'b' }
-            shape_binary: { fg: '#ba8baf' attr: 'b' }
-            shape_block: { fg: '#7cafc2' attr: 'b' }
-            shape_bool: '#86c1b9'
-            shape_closure: { fg: '#86c1b9' attr: 'b' }
-            shape_custom: '#a1b56c'
-            shape_datetime: { fg: '#86c1b9' attr: 'b' }
-            shape_directory: '#86c1b9'
-            shape_external: '#86c1b9'
-            shape_external_resolved: '#86c1b9'
-            shape_externalarg: { fg: '#a1b56c' attr: 'b' }
-            shape_filepath: '#86c1b9'
-            shape_flag: { fg: '#7cafc2' attr: 'b' }
-            shape_float: { fg: '#ab4642' attr: 'b' }
-            shape_garbage: { fg: '#FFFFFF' bg: '#FF0000' attr: 'b' }
-            shape_glob_interpolation: { fg: '#86c1b9' attr: 'b' }
-            shape_globpattern: { fg: '#86c1b9' attr: 'b' }
-            shape_int: { fg: '#ba8baf' attr: 'b' }
-            shape_internalcall: { fg: '#86c1b9' attr: 'b' }
-            shape_keyword: { fg: '#ba8baf' attr: 'b' }
-            shape_list: { fg: '#86c1b9' attr: 'b' }
-            shape_literal: '#7cafc2'
-            shape_match_pattern: '#a1b56c'
-            shape_matching_brackets: { attr: 'u' }
-            shape_nothing: '#ab4642'
-            shape_operator: '#f7ca88'
-            shape_or: { fg: '#ba8baf' attr: 'b' }
-            shape_pipe: { fg: '#ba8baf' attr: 'b' }
-            shape_range: { fg: '#f7ca88' attr: 'b' }
-            shape_raw_string: { fg: '#f8f8f8' attr: 'b' }
-            shape_record: { fg: '#86c1b9' attr: 'b' }
-            shape_redirection: { fg: '#ba8baf' attr: 'b' }
-            shape_signature: { fg: '#a1b56c' attr: 'b' }
-            shape_string: '#a1b56c'
-            shape_string_interpolation: { fg: '#86c1b9' attr: 'b' }
-            shape_table: { fg: '#7cafc2' attr: 'b' }
-            shape_vardecl: { fg: '#7cafc2' attr: 'u' }
-            shape_variable: '#ba8baf'
-
-            foreground: '#d8d8d8'
-            background: '#181818'
-            cursor: '#d8d8d8'
-
-            empty: '#7cafc2'
-            header: { fg: '#d8d8d8' attr: 'b' }
-            hints: '#585858'
-            leading_trailing_space_bg: { attr: 'n' }
-            row_index: { fg: '#d8d8d8' attr: 'b' }
-            search_result: { fg: '#ab4642' bg: '#d8d8d8' }
-            separator: '#d8d8d8'
-          }
+        datetime: {|| (date now) - $in |
+            if $in < 1hr {
+                { fg: '#7c7c7c' attr: 'b' }
+            } else if $in < 6hr {
+                '#7c7c7c'
+            } else if $in < 1day {
+                '#a0a0a0'
+            } else if $in < 3day {
+                '#8e8e8e'
+            } else if $in < 1wk {
+                { fg: '#8e8e8e' attr: 'b' }
+            } else if $in < 6wk {
+                '#868686'
+            } else if $in < 52wk {
+                '#686868'
+            } else { 'dark_gray' }
         }
 
-        $env.config.color_config = (color_theme)
-	$env.LS_COLORS = ""
-        $env.config.buffer_editor = [ "emacs" "-nw" ]
+        filesize: {|e|
+            if $e == 0b {
+                '#b9b9b9'
+            } else if $e < 1mb {
+                '#868686'
+            } else {{ fg: '#686868' }}
+        }
+
+        shape_and: { fg: '#747474' attr: 'b' }
+        shape_binary: { fg: '#747474' attr: 'b' }
+        shape_block: { fg: '#686868' attr: 'b' }
+        shape_bool: '#868686'
+        shape_closure: { fg: '#868686' attr: 'b' }
+        shape_custom: '#8e8e8e'
+        shape_datetime: { fg: '#868686' attr: 'b' }
+        shape_directory: '#868686'
+        shape_external: '#868686'
+        shape_external_resolved: '#868686'
+        shape_externalarg: { fg: '#8e8e8e' attr: 'b' }
+        shape_filepath: '#868686'
+        shape_flag: { fg: '#686868' attr: 'b' }
+        shape_float: { fg: '#7c7c7c' attr: 'b' }
+        shape_garbage: { fg: '#FFFFFF' bg: '#FF0000' attr: 'b' }
+        shape_glob_interpolation: { fg: '#868686' attr: 'b' }
+        shape_globpattern: { fg: '#868686' attr: 'b' }
+        shape_int: { fg: '#747474' attr: 'b' }
+        shape_internalcall: { fg: '#868686' attr: 'b' }
+        shape_keyword: { fg: '#747474' attr: 'b' }
+        shape_list: { fg: '#868686' attr: 'b' }
+        shape_literal: '#686868'
+        shape_match_pattern: '#8e8e8e'
+        shape_matching_brackets: { attr: 'u' }
+        shape_nothing: '#7c7c7c'
+        shape_operator: '#a0a0a0'
+        shape_or: { fg: '#747474' attr: 'b' }
+        shape_pipe: { fg: '#747474' attr: 'b' }
+        shape_range: { fg: '#a0a0a0' attr: 'b' }
+        shape_raw_string: { fg: '#f7f7f7' attr: 'b' }
+        shape_record: { fg: '#868686' attr: 'b' }
+        shape_redirection: { fg: '#747474' attr: 'b' }
+        shape_signature: { fg: '#8e8e8e' attr: 'b' }
+        shape_string: '#8e8e8e'
+        shape_string_interpolation: { fg: '#868686' attr: 'b' }
+        shape_table: { fg: '#686868' attr: 'b' }
+        shape_vardecl: { fg: '#686868' attr: 'u' }
+        shape_variable: '#747474'
+
+        foreground: '#b9b9b9'
+        background: '#101010'
+        cursor: '#b9b9b9'
+
+        empty: '#686868'
+        header: { fg: '#8e8e8e' attr: 'b' }
+        hints: '#525252'
+        leading_trailing_space_bg: { attr: 'n' }
+        row_index: { fg: '#8e8e8e' attr: 'b' }
+        search_result: { fg: '#7c7c7c' bg: '#b9b9b9' }
+        separator: '#b9b9b9'
+    }
+}
+
+$env.config.color_config = (color_theme)
+$env.LS_COLORS = ""
+$env.config.buffer_editor = [ "emacs" "-nw" ]
       '';
     };
     settings = {
@@ -164,7 +164,7 @@
 	x: builtins.attrValues (lib.attrsets.filterAttrs (
 	  n: v: if (builtins.toString n) == "tree-sitter-quint" then false else true)
 	  x)))
-      epkgs.use-package epkgs.nushell-mode epkgs.powershell epkgs.edit-indirect
+      epkgs.use-package epkgs.powershell epkgs.edit-indirect
       epkgs.helm epkgs.treesit-auto epkgs.doom-modeline epkgs.magit
       epkgs.blacken epkgs.flycheck epkgs.yasnippet epkgs.nix-ts-mode
       epkgs.yaml-mode epkgs.yasnippet-capf epkgs.nerd-icons
